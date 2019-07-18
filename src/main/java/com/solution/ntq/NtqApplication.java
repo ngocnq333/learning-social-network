9336e1ee3563050d13e4a8a1ec967dc4aabfa135
Duc Phan <duc.phan@ntq-solution.com.vn>
2019-07-18 09:25:06 +0700

    Initial Project

---
+++ src/main/java/com/solution/ntq/NtqApplication.java
@@ -0,0 +1,13 @@
+package com.solution.ntq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NtqApplication.class, args);
+    }
+
+}
