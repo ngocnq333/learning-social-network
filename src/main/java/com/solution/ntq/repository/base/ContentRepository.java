994fd0948f2a40c188c15a7eb485567c2dbc8a0c
DucAnh <anh.pham.intern@ntq-solution.com.vn>
2019-08-16 10:34:06 +0700

    init list pending content
    approve for content
    
    comment

--- src/main/java/com/solution/ntq/repository/base/ContentRepository.java
+++ src/main/java/com/solution/ntq/repository/base/ContentRepository.java
@@ -18,8 +18,11 @@ public interface ContentRepository extends Repository<Content,Integer> {
 
     Content findContentById(int idContent);
 
-    @Query(value = "SELECT * FROM Content c WHERE c.clazz_id = ?1 ORDER BY c.is_done ASC ,c.end_date DESC , c.start_date DESC ", nativeQuery = true)
-    List<Content> findContentByIdClazz(int classId);
+    @Query(value = "SELECT * FROM Content c WHERE c.clazz_id = ?1 AND is_approve =1 ORDER BY c.is_done ASC ,c.end_date DESC , c.start_date DESC ", nativeQuery = true)
+    List<Content> findContentApproveByIdClazz(int classId);
+    @Query(value = "SELECT * FROM Content c WHERE c.clazz_id = ?1 AND is_approve =0 ORDER BY c.is_done ASC ,c.end_date DESC , c.start_date DESC ", nativeQuery = true)
+    List<Content> findContentNotApproveByIdClazz(int classId);
+
 
     @Transactional
     void deleteById(int idContent);
